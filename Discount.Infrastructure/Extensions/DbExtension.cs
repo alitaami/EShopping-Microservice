d2f2ddd0bd45be0d7c962be34db867a480a55520
Ali Taami <alitaami81@gmail.com>
2023-10-29 22:18:03 +0330

    Changed the  DbExtension

--- Discount.Infrastructure/Extensions/DbExtension.cs
+++ Discount.Infrastructure/Extensions/DbExtension.cs
@@ -1,56 +1,76 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
 using Npgsql;
+using System;
+using System.Data;
+using System.Threading.Tasks;
 
-namespace Discount.Infrastructure.Extensions;
-
-public static class DbExtension
+public class DbExtension
 {
-    public static IHost MigrateDatabase<TContext>(this IHost host)
-    {
-        using (var scope = host.Services.CreateScope())
-        {
-            var services = scope.ServiceProvider;
-            var config = services.GetRequiredService<IConfiguration>();
-            var logger = services.GetRequiredService<ILogger<TContext>>();
-            try
-            {
-                logger.LogInformation("Discount DB Migration Started");
-                ApplyMigrations(config);
-                logger.LogInformation("Discount DB Migration Completed");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-        }
+    private readonly string _connectionString;
 
-        return host;
+    public DbExtension(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
     }
 
-    private static void ApplyMigrations(IConfiguration config)
+    public async Task RecreateCouponTableAsync()
     {
-        using var connection = new NpgsqlConnection(config.GetValue<string>("DatabaseSettings:ConnectionString"));
-        connection.Open();
-        using var cmd = new NpgsqlCommand()
+        using IDbConnection db = new NpgsqlConnection(_connectionString);
+
+        try
         {
-            Connection = connection
-        };
-        cmd.CommandText = "DROP TABLE IF EXISTS Coupon";
-        cmd.ExecuteNonQuery();
-        cmd.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
-                                                ProductName VARCHAR(500) NOT NULL,
-                                                Description TEXT,
-                                                Amount INT)";
-        cmd.ExecuteNonQuery();
+            // Check if the table exists
+            if (await TableExistsAsync(db, "Coupon"))
+            {
+                // Drop the table if it exists
+                await DropCouponTableAsync(db);
+            }
 
-        cmd.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Adidas Quick Force Indoor Badminton Shoes', 'Shoe Discount', 500);";
-        cmd.ExecuteNonQuery();
+            // Create the table
+            await CreateCouponTableAsync(db);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            // Optionally, you can throw the exception to be handled by the caller or log it for further analysis.
+            throw;
+        }
+    }
 
-        cmd.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Yonex VCORE Pro 100 A Tennis Racquet (270gm, Strung)', 'Racquet Discount', 700);";
-        cmd.ExecuteNonQuery();
+    private async Task<bool> TableExistsAsync(IDbConnection db, string tableName)
+    {
+        var result = await db.ExecuteScalarAsync<int>(
+            "SELECT COUNT(*) FROM information_schema.tables WHERE table_name = @TableName",
+            new { TableName = tableName.ToLower() }
+        );
+        return result > 0;
+    }
+
+    private async Task DropCouponTableAsync(IDbConnection db)
+    {
+        var dropTableSql = "DROP TABLE Coupon";
+        await db.ExecuteAsync(dropTableSql);
+        Console.WriteLine("Coupon table dropped successfully.");
+    }
+
+    private async Task CreateCouponTableAsync(IDbConnection db)
+    {
+        var createTableSql = @"
+        CREATE TABLE Coupon (
+            Id SERIAL PRIMARY KEY,
+            ProductName VARCHAR(500) NOT NULL,
+            Description TEXT,
+            Amount INT NOT NULL
+        )";
+        await db.ExecuteAsync(createTableSql);
+
+        var command = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Adidas Quick Force Indoor Badminton Shoes', 'Shoe Discount', 500);";
+        await db.ExecuteAsync(command);
+
+        var command1 = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Yonex VCORE Pro 100 A Tennis Racquet (270gm, Strung)', 'Racquet Discount', 700);";
+        await db.ExecuteAsync(command1);
+
+        Console.WriteLine("Coupon table created successfully.");
     }
 }
