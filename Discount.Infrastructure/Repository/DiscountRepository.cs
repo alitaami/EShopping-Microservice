a71480396603a0387e6f2417fdfe919e621ca372
Ali Taami <alitaami81@gmail.com>
2023-10-29 13:38:02 +0330

    Implemented  DiscountRepository methods

--- Discount.Infrastructure/Repository/DiscountRepository.cs
+++ Discount.Infrastructure/Repository/DiscountRepository.cs
@@ -1,30 +1,110 @@
-﻿using Discount.Core.Entities;
+﻿using Dapper;
+using Discount.Common.Resources;
+using Discount.Core.Entities;
 using Entities.Base;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using NLog;
+using Npgsql;
 
-public class DiscountRepository :ServiceBase<DiscountRepository> , IDiscountRepository
+public class DiscountRepository : ServiceBase<DiscountRepository>, IDiscountRepository
 {
-    public DiscountRepository(ILogger<DiscountRepository> logger) : base(logger)
-    { 
-    }
-    public Task<ServiceResult> CreateDiscount(Coupon coupon)
+    private readonly IConfiguration _configuration;
+    public DiscountRepository(IConfiguration configuration, ILogger<DiscountRepository> logger) : base(logger)
     {
-        throw new NotImplementedException();
+        _configuration = configuration;
+    }
+    public async Task<ServiceResult> CreateDiscount(Coupon coupon)
+    {
+        try
+        {
+            await using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var affected =
+                await connection.ExecuteAsync
+                ("INSERT INTO Coupon (ProductName , Description , Amount) VALUES (@ProductName ,@Description , @Amount)",
+                new{ ProductName = coupon.ProductName , Description = coupon.Description , Amount = coupon.Amount });
+
+            if (affected == 0)
+                return BadRequest(ErrorCodeEnum.BadRequest, Resource.CreateError, null);///
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, null, null);
+
+            return InternalServerError(ErrorCodeEnum.InternalError, Resource.GeneralErrorTryAgain, null);
+        }
     }
 
-    public Task<ServiceResult> DeleteDiscount(string productName)
+    public async Task<ServiceResult> DeleteDiscount(string productName)
     {
-        throw new NotImplementedException();
+        try
+        {
+            await using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var affected =
+                 await connection.ExecuteAsync
+                 ("DELETE FROM Coupon WHERE ProductName = @ProductName",
+                 new { ProductName = productName });
+
+            if (affected == 0)
+                return BadRequest(ErrorCodeEnum.BadRequest, Resource.DeleteError, null);///
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, null, null);
+
+            return InternalServerError(ErrorCodeEnum.InternalError, Resource.GeneralErrorTryAgain, null);
+        }
     }
 
-    public Task<ServiceResult> GetDiscount(string productName)
+    public async Task<ServiceResult> GetDiscount(string productName)
     {
-        throw new NotImplementedException();
+        try
+        {
+            await using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>
+                ("SELECT * FROM Coupon WHERE ProductName = @ProductName", new { ProductName = productName });
+
+            if (coupon == null)
+                return NotFound(ErrorCodeEnum.NotFound, Resource.NotFound, null);///
+
+            return Ok(coupon);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, null, null);
+
+            return InternalServerError(ErrorCodeEnum.InternalError, Resource.GeneralErrorTryAgain, null);
+        }
     }
 
-    public Task<ServiceResult> UpdateDiscount(Coupon coupon)
+    public async Task<ServiceResult> UpdateDiscount(Coupon coupon)
     {
-        throw new NotImplementedException();
+         try
+        {
+            await using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var affected =
+                await connection.ExecuteAsync
+                 ("UPDATE Coupon SET ProductName = @ProductName , Description =@Description , Amount =@Amount WHERE Id = @Id",
+                new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount, Id = coupon.Id });
+
+            if (affected == 0)
+                return BadRequest(ErrorCodeEnum.BadRequest, Resource.UpdateError, null);///
+
+            return Ok(coupon);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, null, null);
+
+            return InternalServerError(ErrorCodeEnum.InternalError, Resource.GeneralErrorTryAgain, null);
+        }
     }
 }
