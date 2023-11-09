5070b985a10261516ecf149acec8805acbe29d65
Ali Taami <alitaami81@gmail.com>
2023-11-09 12:21:46 +0330

    Fixed the proto problem in basket.application

---
+++ Basket.Application/GrpcService/DiscountGrpcService.cs
@@ -0,0 +1,23 @@
+﻿using Discount.Grpc.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Basket.Application.GrpcService
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoServiceClient;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoServiceClient)
+        {
+            _discountProtoServiceClient = discountProtoServiceClient ?? throw new ArgumentNullException(nameof(discountProtoServiceClient));
+        }
+
+        // Your methods here
+    }
+
+}
