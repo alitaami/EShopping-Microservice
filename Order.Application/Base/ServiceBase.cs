3709e29ac1835d8fce11916145c60b296c93a6da
Ali Taami <alitaami81@gmail.com>
2023-12-07 18:17:11 +0330

    Added some common things and SeedData class

--- Order.Application/Base/ServiceBase.cs
+++ Order.Application/Base/ServiceBase.cs
@@ -1,130 +1,182 @@
-﻿using Discount.Common.Resources;
-using Entities.Base;
+﻿
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using Npgsql; // Add this for PostgreSQL support
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Net;
+using Entities.Base;
+using Microsoft.IdentityModel.Tokens;
+using Order.Common.Resources;
 
-public class ServiceBase<Tclass>
+namespace EstateAgentApi.Services.Base
 {
-    protected readonly ILogger<Tclass> _logger;
-
-    public ServiceBase(ILogger<Tclass> logger)
+    public class ServiceBase<Tclass>
     {
-        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-    }
-
-    protected virtual ServiceResult HandleException(Exception ex)
-    {
-        try
+        protected readonly ILogger<Tclass> _logger;
+        public ServiceBase(
+           ILogger<Tclass> logger)
         {
-            if (ex is PostgresException pgEx) // Handle PostgreSQL-specific exceptions
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        protected virtual ServiceResult HandleException(Exception ex)
+        {
+            try
             {
-                return HandlePostgresException(pgEx);
+                if (ex is DbUpdateException)
+                {
+                    return HandleDbUpdateException(ex);
+                }
+                else if (ex is APIException napEx)
+                {
+                    return HandleAPIException(napEx);
+                }
+                else
+                {
+                    return InternalServerError(ErrorCodeEnum.None, Resource.GeneralErrorTryAgain, null);
+                }
             }
-            else if (ex is APIException napEx)
+            catch (Exception generalEx)
             {
-                return HandleAPIException(napEx);
+                _logger.LogError(generalEx, null);
+
+                throw; //Todo : درسته ایا یا باید همینجا هندل بشه
+            }
+        }
+
+        protected virtual ServiceResult Ok(object data)
+        {
+            if (data == null)
+            {
+                return Ok();
+            }
+
+            return new ServiceResult(data, new ApiResult(HttpStatusCode.OK, ErrorCodeEnum.None, null, null));
+        }
+
+        protected virtual ServiceResult Ok()
+        {
+            return new ServiceResult(null, new ApiResult(HttpStatusCode.OK, ErrorCodeEnum.None, null, null));
+        }
+
+        protected virtual ServiceResult BadRequest(ErrorCodeEnum errorCode, string errorMessage, List<FieldErrorItem> errors)
+        {
+            return new ServiceResult(null, new ApiResult(HttpStatusCode.BadRequest, errorCode, errorMessage, errors));
+        }
+
+        protected virtual ServiceResult NotFound(ErrorCodeEnum errorCode, string errorMessage, List<FieldErrorItem> errors)
+        {
+            return new ServiceResult(null, new ApiResult(HttpStatusCode.NotFound, errorCode, errorMessage, errors));
+        }
+
+        protected virtual ServiceResult InternalServerError(ErrorCodeEnum errorCode, string errorMessage, List<FieldErrorItem> errors)
+        {
+            return new ServiceResult(null, new ApiResult(HttpStatusCode.InternalServerError, errorCode, errorMessage, errors));
+        }
+        protected virtual void ValidateModel(object model)
+        {
+            if (model is null)
+                throw new ArgumentNullException(nameof(model));
+
+            var validationResults = new List<ValidationResult>();
+            if (!Validator.TryValidateObject(model, new ValidationContext(model), validationResults, true))
+                throw new APIException(HttpStatusCode.BadRequest, Resource.EnterParametersCorrectlyAndCompletely, validationResults.AsReadOnly());
+        }
+
+        private ServiceResult HandleDbUpdateException(Exception ex)
+        {
+            var sqlEx = ex.InnerException as SqlException;
+            if (sqlEx != null && (sqlEx.Number == 2601 || sqlEx.Number == 2627))  /// unique constraint violation
+            {
+                //TODO: ErrorCode اضافه و مشخص شود
+                return BadRequest(ErrorCodeEnum.None, "به دلیل نقض یونیک بودن بعضی از فیلدها امکان اضافه شدن و یا اپدیت وجود ندارد ", null);
+            }
+            else if (sqlEx != null && (sqlEx.Number == 547))  /// Foreign key constraint violation
+            {
+                return BadRequest(ErrorCodeEnum.None, "به دلیل نقض محدودیت کلید خارجی امکان درج یا آپدیت وجود ندارد", null);
             }
             else
             {
-                return InternalServerError(ErrorCodeEnum.None, Resource.GeneralErrorTryAgain, null);
+                return InternalServerError(ErrorCodeEnum.None, "", null);
             }
         }
-        catch (Exception generalEx)
+
+        private ServiceResult HandleAPIException(APIException napEx)
         {
-            _logger.LogError(generalEx, null);
-            throw;
+            if (napEx is null)
+                throw new ArgumentNullException(nameof(napEx));
+
+            if (napEx.ValidationResults.IsNullOrEmpty())
+                return BadRequest(ErrorCodeEnum.None, "خطای اعتبارسنجی فیلدها", null);
+
+            var validationResults = new List<FieldErrorItem>();
+            napEx.ValidationResults
+                .ToList()
+                .ForEach(v =>
+                    validationResults
+                    .Add(new FieldErrorItem(v.MemberNames.FirstOrDefault() ?? string.Empty, new List<string>() { v.ErrorMessage ?? string.Empty })));
+
+            return BadRequest(ErrorCodeEnum.None, "خطای اعتبارسنجی فیلدها", validationResults);
         }
-    }
 
-    protected virtual ServiceResult Ok(object data)
-    {
-        if (data == null)
-        {
-            return Ok();
-        }
+        //protected virtual ApiResultService HandleException(Exception ex)
+        //{
+        //    try
+        //    {
+        //        if (ex is DbUpdateException)
+        //        {
+        //            return HandleDbUpdateException(ex);
+        //        }
+        //        else if (ex is NAPException napEx)
+        //        {
+        //            return HandleNAPException(napEx);
+        //        }
+        //        else
+        //        {
+        //            return InternalServerError(ErrorCodeEnum.None, ValidationResource.GeneralErrorTryAgain, null);
+        //        }
+        //    }
+        //    catch (Exception generalEx)
+        //    {
+        //        _logger.LogError(generalEx, null);
 
-        return new ServiceResult(data, new ApiResult(HttpStatusCode.OK, ErrorCodeEnum.None, null, null));
-    }
+        //        throw; //Todo : درسته ایا یا باید همینجا هندل بشه
+        //    }
+        //}
 
-    protected virtual ServiceResult Ok()
-    {
-        return new ServiceResult(null, new ApiResult(HttpStatusCode.OK, ErrorCodeEnum.None, null, null));
-    }
+        //private ApiResultService HandleDbUpdateException(Exception ex)
+        //{
+        //    var sqlEx = ex.InnerException as SqlException;
+        //    if (sqlEx != null && (sqlEx.Number == 2601 || sqlEx.Number == 2627))  /// unique constraint violation
+        //    {
+        //        //TODO: ErrorCode اضافه و مشخص شود
+        //        return BadRequest(ErrorCodeEnum.None, "به دلیل نقض یونیک بودن بعضی از فیلدها امکان اضافه شدن و یا اپدیت وجود ندارد ", null);
+        //    }
+        //    else if (sqlEx != null && (sqlEx.Number == 547))  /// Foreign key constraint violation
+        //    {
+        //        return BadRequest(ErrorCodeEnum.None, "به دلیل نقض محدودیت کلید خارجی امکان درج یا آپدیت وجود ندارد", null);
+        //    }
+        //    else
+        //    {
+        //        return InternalServerError(ErrorCodeEnum.None, "", null);
+        //    }
+        //}
 
-    protected virtual ServiceResult BadRequest(ErrorCodeEnum errorCode, string errorMessage, List<FieldErrorItem> errors)
-    {
-        return new ServiceResult(null, new ApiResult(HttpStatusCode.BadRequest, errorCode, errorMessage, errors));
-    }
+        //private ApiResultService HandleNAPException(NAPException napEx)
+        //{
+        //    if (napEx is null)
+        //        throw new ArgumentNullException(nameof(napEx));
 
-    protected virtual ServiceResult NotFound(ErrorCodeEnum errorCode, string errorMessage, List<FieldErrorItem> errors)
-    {
-        return new ServiceResult(null, new ApiResult(HttpStatusCode.NotFound, errorCode, errorMessage, errors));
-    }
+        //    if (napEx.ValidationResults.IsNullOrEmpty())
+        //        return BadRequest(ErrorCodeEnum.None, "خطای اعتبارسنجی فیلدها", null);
 
-    protected virtual ServiceResult InternalServerError(ErrorCodeEnum errorCode, string errorMessage, List<FieldErrorItem> errors)
-    {
-        return new ServiceResult(null, new ApiResult(HttpStatusCode.InternalServerError, errorCode, errorMessage, errors));
-    }
+        //    var validationResults = new List<FieldErrorItem>();
+        //    napEx.ValidationResults
+        //        .ToList()
+        //        .ForEach(v =>
+        //            validationResults
+        //            .Add(new FieldErrorItem(v.MemberNames.FirstOrDefault() ?? string.Empty, new List<string>() { v.ErrorMessage ?? string.Empty })));
 
-    protected virtual void ValidateModel(object model)
-    {
-        if (model is null)
-            throw new ArgumentNullException(nameof(model));
-
-        var validationResults = new List<ValidationResult>();
-        if (!Validator.TryValidateObject(model, new ValidationContext(model), validationResults, true))
-            throw new APIException(HttpStatusCode.BadRequest, Resource.EnterParametersCorrectlyAndCompletely, validationResults.AsReadOnly());
-    }
-
-    protected virtual ServiceResult HandlePostgresException(PostgresException pgEx)
-    {
-        // Handle specific PostgreSQL errors. For example, unique constraint violations.
-        if (pgEx.SqlState == "23505") // Unique constraint violation
-        {
-            return new ServiceResult(
-                null,
-                new ApiResult(
-                    HttpStatusCode.BadRequest,
-                    ErrorCodeEnum.DuplicateKey,
-                    "Duplicate entry detected.",
-                    null
-                )
-            );
-        }
-        else
-        {
-            return new ServiceResult(
-                null,
-                new ApiResult(
-                    HttpStatusCode.InternalServerError,
-                    ErrorCodeEnum.DatabaseWriteError,
-                    "An error occurred while writing to the database.",
-                    null
-                )
-            );
-        }
-    }
-
-    private ServiceResult HandleAPIException(APIException napEx)
-    {
-        if (napEx is null)
-            throw new ArgumentNullException(nameof(napEx));
-
-        if (napEx.ValidationResults == null)
-            return BadRequest(ErrorCodeEnum.None, "خطای اعتبارسنجی فیلدها", null);
-
-        var validationResults = new List<FieldErrorItem>();
-        napEx.ValidationResults
-            .ToList()
-            .ForEach(v =>
-                validationResults
-                .Add(new FieldErrorItem(v.MemberNames.FirstOrDefault() ?? string.Empty, new List<string>() { v.ErrorMessage ?? string.Empty })));
-
-        return BadRequest(ErrorCodeEnum.None, "خطای اعتبارسنجی فیلدها", validationResults);
+        //    return BadRequest(ErrorCodeEnum.None, "خطای اعتبارسنجی فیلدها", validationResults);
+        //}
     }
 }
---
+++ Order.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetOrders());
+                await orderContext.SaveChangesAsync(CancellationToken.None);
+                logger.LogInformation($"Ordering Database: {typeof(OrderContext).Name} seeded.");
+            }
+        }
+
+        private static IEnumerable<Core.Entities.Order> GetOrders()
+        {
+            return new List<Core.Entities.Order>
+        {
+            new()
+            {
+                UserName = "rahul",
+                FirstName = "Rahul",
+                LastName = "Sahay",
+                EmailAddress = "rahulsahay@eshop.net",
+                AddressLine = "Bangalore",
+                Country = "India",
+                TotalPrice = 750,
+                State = "KA",
+                ZipCode = "560001",
+                CreatedDate  = new DateTime(),
+                CardName = "Visa",
+                CardNumber = "1234567890123456",
+                CreatedBy = "Rahul",
+                Expiration = "12/25",
+                Cvv = "123",
+                PaymentMethod = 1,
+                LastModifiedBy = "Rahul",
+                LastModifiedDate = new DateTime(),
+            }
+        };
+        }
+    }
+}
