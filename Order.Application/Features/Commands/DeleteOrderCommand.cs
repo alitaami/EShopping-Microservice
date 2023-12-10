cf61da16db7a1ab9acbd9a593f39b44fbf065552
Ali Taami <alitaami81@gmail.com>
2023-12-10 21:54:05 +0330

    Added DeleteOrderCommand

---
+++ Order.Application/Features/Commands/DeleteOrderCommand.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Entities.Base;
+using EstateAgentApi.Services.Base;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Order.Common.Resources;
+using Order.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket.Application.Features.Commands
+{
+    public class DeleteOrderCommand : IRequest<ServiceResult>
+    {
+        public int Id { get; set; }
+        public DeleteOrderCommand(int ID)
+        {
+            Id = ID;
+        }
+
+        public class DeleteOrderCommandHandler : ServiceBase<DeleteOrderCommandHandler>, IRequestHandler<DeleteOrderCommand, ServiceResult>
+        {
+            private readonly IAsyncRepository<Order.Core.Entities.Order> _order;
+            public DeleteOrderCommandHandler(ILogger<DeleteOrderCommandHandler> logger, IAsyncRepository<Order.Core.Entities.Order> order) : base(logger)
+            {
+                _order = order;
+            }
+            public async Task<ServiceResult> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+            {
+                try
+                {
+                    var order = await _order.GetByIdAsync(request.Id);
+
+                    if (order is null)
+                        return BadRequest(ErrorCodeEnum.BadRequest, Resource.NotFound, null);
+
+                    await _order.DeleteAsync(order);
+
+                    return Ok();
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, null, null);
+                    return InternalServerError(ErrorCodeEnum.InternalError, Resource.GeneralErrorTryAgain, null);
+                }
+            }
+        }
+    }
+}
