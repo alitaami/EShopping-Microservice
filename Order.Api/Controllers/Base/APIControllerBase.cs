d0d0f6754d3dc196bee1ec8b8c7f4110b220fb96
Ali Taami <alitaami81@gmail.com>
2023-12-18 01:40:24 +0330

    Added OrderController

---
+++ Order.Api/Controllers/Base/APIControllerBase.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Order.Common.Resources;
+using Entities.Base;
+
+namespace Catalog.Api.Controllers
+{
+
+    //[Route("api/[controller]/[action]")]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]/[action]")]  // api/v1/[controller]
+    public class APIControllerBase : ControllerBase
+    {
+        protected IActionResult APIResponse(ServiceResult serviceResult)
+        {
+            if (serviceResult.Result.HttpStatusCode == (int)HttpStatusCode.OK)
+            {
+                if (serviceResult.Data == null)
+                    return Ok();
+                else
+                    return Ok(serviceResult.Data);
+            }
+
+            else if (serviceResult.Result.HttpStatusCode == (int)HttpStatusCode.BadRequest)
+                return BadRequest(serviceResult.Result);
+
+            else if (serviceResult.Result.HttpStatusCode == (int)HttpStatusCode.NotFound)
+                return NotFound(serviceResult.Result);
+
+            else if (serviceResult.Result.HttpStatusCode == (int)HttpStatusCode.InternalServerError)
+                return StatusCode((int)HttpStatusCode.InternalServerError, serviceResult.Data);
+
+            else //TODO : این مورد بررسی بشه شاید نیاز به تغییر باشه
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+        }
+
+        protected IActionResult InternalServerError()
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError,
+                    new ApiResult(HttpStatusCode.InternalServerError,
+                    ErrorCodeEnum.InternalError,
+                    Resource.GeneralErrorTryAgain
+                    , null));
+        }
+
+        protected IActionResult InternalServerError(ErrorCodeEnum error)
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError,
+                    new ApiResult(HttpStatusCode.InternalServerError,
+                    error,
+                    Resource.GeneralErrorTryAgain
+                    , null));
+        }
+
+        protected IActionResult InternalServerError(string message)
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError,
+                    new ApiResult(HttpStatusCode.InternalServerError,
+                    ErrorCodeEnum.InternalError,
+                    message
+                    , null));
+        }
+
+        protected IActionResult InternalServerError(ErrorCodeEnum error, string message)
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError,
+                    new ApiResult(HttpStatusCode.InternalServerError,
+                    error,
+                    message
+                    , null));
+        }
+    }
+}
---
+++ Order.Api/Controllers/OrderApiController.cs
@@ -0,0 +1,116 @@
+﻿using Entities.Base;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using System.Net;
+
+using Order.Application.Features.Queries;
+using Order.Common.Resources;
+using Order.Application.ViewModels;
+using Order.Application.Features.Commands;
+using Basket.Application.Features.Commands;
+
+namespace Catalog.Api.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [ApiVersion("1")]
+    [AllowAnonymous]
+    public class OrderApiController : APIControllerBase
+    {
+        private readonly ILogger<OrderApiController> _logger;
+        private ISender _sender;
+        public OrderApiController(ILogger<OrderApiController> logger, ISender sender)
+        {
+            _sender = sender;
+            _logger = logger;
+        }
+
+        /// <summary>
+        ///  Get the product by its Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{username}")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(ApiResult), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ApiResult), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(ApiResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> GetOrderByUsername(string username)
+        {
+            try
+            {
+                var res = await _sender.Send(new GetOrdersListQuery(username));
+
+                return APIResponse(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, null, null);
+                return InternalServerError(ErrorCodeEnum.InternalError, Resource.GeneralErrorTryAgain);
+            }
+        }
+        [HttpPost("id")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(ApiResult), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ApiResult), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(ApiResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> DeleteOrder(int id  )
+        {
+            try
+            {
+                var res = await _sender.Send(new DeleteOrderCommand(id));
+
+                return APIResponse(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, null, null);
+                return InternalServerError(ErrorCodeEnum.InternalError, Resource.GeneralErrorTryAgain);
+            }
+        }
+
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(ApiResult), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ApiResult), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(ApiResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> CheckoutOrder([FromBody] CheckoutOrderViewModel model)
+        {
+            try
+            {
+                var res = await _sender.Send(new CheckoutOrderCommand(model));
+
+                return APIResponse(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, null, null);
+                return InternalServerError(ErrorCodeEnum.InternalError, Resource.GeneralErrorTryAgain);
+            }
+        }
+
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(ApiResult), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ApiResult), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(ApiResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> UpdateOrder([FromBody] UpdateOrderViewModel model)
+        {
+            try
+            {
+                var res = await _sender.Send(new UpdateOrderCommand(model));
+
+                return APIResponse(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, null, null);
+                return InternalServerError(ErrorCodeEnum.InternalError, Resource.GeneralErrorTryAgain);
+            }
+        }
+
+    }
+}
