f29665673b5d7bbac740d345421e18d63b779920
Ali Taami <alitaami81@gmail.com>
2023-12-24 13:40:44 +0330

    Added refrences to EventBus from Basket service

--- EventBus.Message/Common/EventBusConstant.cs
+++ EventBus.Message/Common/EventBusConstant.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace EventBus.Message.Common
 {
-    internal class EventBusConstant
+    public class EventBusConstant
     {
+        public const string BasketCheckoutQueue = "basketcheckout-queue";
     }
 }
--- EventBus.Message/Events/BasketChekoutEvent.cs
+++ EventBus.Message/Events/BasketChekoutEvent.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EventBus.Message.Events
 {
-    internal class BasketChekoutEvent
+    public class BasketChekoutEvent :BaseIntegrationEvent
     {
+        public string? UserName { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? TotalPrice { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? AddressLine { get; set; }
+        public string? Country { get; set; }
+        public string? State { get; set; }
+        public string? ZipCode { get; set; }
+        public string? CardName { get; set; }
+        public string? CardNumber { get; set; }
+        public string? Expiration { get; set; }
+        public string? Cvv { get; set; }
+        public int? PaymentMethod { get; set; }
     }
 }
