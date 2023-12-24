1efdfd67e9898f03adee9f5f502da530e1aa854d
Ali Taami <alitaami81@gmail.com>
2023-12-24 13:29:00 +0330

    Added EventBus project

---
+++ EventBus.Message/Events/BaseIntegrationEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Message.Events
+{
+    public class BaseIntegrationEvent
+    {
+        //Co-Relation Id
+        public Guid Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public BaseIntegrationEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.Now;
+        }
+        public BaseIntegrationEvent(Guid id , DateTime creationDate)
+        {
+                Id = id;
+            CreationDate = creationDate;
+        }
+                
+    }
+}
