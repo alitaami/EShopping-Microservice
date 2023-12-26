1e6c3152c71c5e93efe65554380efb9024a4dceb
Ali Taami <alitaami81@gmail.com>
2023-12-26 12:02:34 +0330

    Added Satrtup class to ApiGateways

--- Ocelot.ApiGateway/Startup.cs
+++ Ocelot.ApiGateway/Startup.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +16,40 @@ using System.Threading.Tasks;
 
 namespace Ocelot.ApiGateway
 {
-    internal class Startup
+    public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot()
+                    .AddCacheManager(o => o.WithDictionaryHandle());
+        }
+        public async void Configure(IApplicationBuilder builder, IWebHostEnvironment env)
+        {
+            var logger = LogManager.Setup().LoadConfigurationFromFile("nlog.config").GetCurrentClassLogger();
+ 
+            try
+            {
+                if (env.IsDevelopment())
+                {
+                    builder.UseDeveloperExceptionPage();
+                }
+
+                builder.UseRouting();
+                builder.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapGet("/", async context =>
+                    {
+                        await context.Response.WriteAsync("hello ocelot");
+                    });
+                });
+
+                await builder.UseOcelot();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+                throw;
+            }
+        }
     }
 }
